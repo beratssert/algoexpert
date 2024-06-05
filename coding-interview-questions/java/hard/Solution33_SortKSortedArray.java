bbb48e8dcbb41deb5bd8f6d0e00ae4e50650445d
Berat Sert <registrationbs@tutanota.com>
2024-06-05 16:53:21 +0300

    sort k sorted array

---
+++ coding-interview-questions/java/hard/Solution33_SortKSortedArray.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+class Program {
+  // O(nlogk) time | O(k) space - n is the length of the array, k is the input k.
+  public int[] sortKSortedArray(int[] array, int k) {
+    List<Integer> heapValues = new ArrayList<>();
+    for (int i = 0; i < Math.min(k + 1, array.length); i++) {
+      heapValues.add(array[i]);
+    }
+
+    MinHeap heap = new MinHeap(heapValues);
+
+    int nextIdxToInsert = 0;
+    for (int i = k + 1; i < array.length; i++) {
+      array[nextIdxToInsert] = heap.remove();
+      nextIdxToInsert++;
+      heap.insert(array[i]);
+    }
+
+    while (!heap.isEmpty()) {
+      array[nextIdxToInsert] = heap.remove();
+      nextIdxToInsert++;
+    }
+
+    return array;
+  }
+
+  static class MinHeap {
+    List<Integer> heap = new ArrayList<Integer>();
+
+    public MinHeap(List<Integer> array) {
+      heap = buildHeap(array);
+    }
+
+    // O(n) time | O(1) space - n is the number of elements in the array.
+    private List<Integer> buildHeap(List<Integer> array) {
+      int lastParentIdx = (array.size() - 2) / 2;
+      for (int i = lastParentIdx; i >= 0; i--) {
+        siftDown(i, array.size() - 1, array);
+      }
+
+      return array;
+    }
+
+    // O(logn) time | O(1) space - n is the number of elements in the array.
+    private void siftDown(int currentIdx, int endIdx, List<Integer> heap) {
+      int childOne = currentIdx * 2 + 1;
+      while (childOne <= endIdx) {
+        int childTwo = currentIdx * 2 + 2;
+        int idxToSwap;
+        if (childTwo <= endIdx && heap.get(childTwo) < heap.get(childOne)) {
+          idxToSwap = childTwo;
+        } else {
+          idxToSwap = childOne;
+        }
+
+        if (heap.get(idxToSwap) < heap.get(currentIdx)) {
+          swap(currentIdx, idxToSwap, heap);
+          currentIdx = idxToSwap;
+          childOne = currentIdx * 2 + 1;
+        } else {
+          return;
+        }
+      }
+    }
+
+    // O(logn) time | O(1) space - n is the number of elements in the array.
+    private void siftUp(int currentIdx, List<Integer> heap) {
+      int parentIdx = (currentIdx - 1) / 2;
+      while (currentIdx > 0 && heap.get(currentIdx) < heap.get(parentIdx)) {
+        swap(currentIdx, parentIdx, heap);
+        currentIdx = parentIdx;
+        parentIdx = (currentIdx - 1) / 2;
+      }
+    }
+
+    public int remove() {
+      swap(0, heap.size() - 1, heap);
+      int removedElement = heap.remove(heap.size() - 1);
+      siftDown(0, heap.size() - 1, heap);
+      return removedElement;
+    }
+
+    public void insert(int value) {
+      heap.add(value);
+      siftUp(heap.size() - 1, heap);
+    }
+
+    public boolean isEmpty() {
+      return heap.size() == 0;
+    }
+
+    private void swap(int i, int j, List<Integer> heap) {
+      int temp = heap.get(i);
+      heap.set(i, heap.get(j));
+      heap.set(j, temp);
+    }
+  }
+}
